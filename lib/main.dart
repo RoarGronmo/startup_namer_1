de291b8444c320197f909dee6a3cfccc0ac2be03
Roar Grønmo <roar.gronmo@gmail.com>
2021-09-19 17:31:34 +0200

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -7,17 +7,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    return MaterialApp(
-      title: "Welcome to Flutter",
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Welcome to Flutter"),
-        ),
-        body: Center(
-          child:RandomWords()
-        ),
-      ),
-    );
+   return Scaffold(
+     appBar: AppBar(
+       title: const Text("Startup Name Generator"),
+     ),
+     body: _buildSuggestions(),
+   );
   }
 }
 
@@ -32,10 +27,35 @@ class RandomWords extends StatefulWidget {
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18.0);
-  
+
   @override
   Widget build(BuildContext context) {
     final wordPair = WordPair.random();
     return Text(wordPair.asPascalCase);
   }
+
+  Widget _buildSuggestions(){
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: /*1*/ (context,i) {
+          if(i.isOdd) return const Divider(); /*2*/
+
+          final index = i ~/2; /*3*/
+          if(index >= _suggestions.length){
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_buildSuggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair){
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
 }
+
+
